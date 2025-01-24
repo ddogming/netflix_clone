01ba49df9dce6f80a8c512581eac19586e3498a5
ddogming <tpdus9170@naver.com>
2025-01-24 14:10:43 +0900

    reupdate

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import "./App.scss";
 import Banner from "./components/Banner";
 import FAQ from "./components/FAQ";
 import Header from "./components/Header";
+import Membership from "./components/Membership";
+import Whyjoin from "./components/Whyjoin";
+import Footer from "./components/Footer";
+import MoveSlider from "./components/MoveSlider";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 const App = () => {
   return (
-    <div className="app">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" element={
+    <div className="app"> 
       <Header />
       <Banner />
+      <MoveSlider />
+      <Whyjoin/>
       <FAQ/>
+      <Membership/>
+      <Footer/>
     </div>
+    }/>
+    <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 };
 
